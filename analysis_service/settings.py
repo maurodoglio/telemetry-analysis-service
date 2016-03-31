2f652c09854d6aa05bccaf78a69eeba3ab378bbb
Anthony Zhang <azhang9@gmail.com>
2016-03-31 18:12:51 -0400

    Travis test 2

--- analysis_service/settings.py
+++ analysis_service/settings.py
@@ -126,8 +126,8 @@ USE_I18N = config('USE_I18N', default=True, cast=bool)
 USE_L10N = config('USE_L10N', default=True, cast=bool)
 USE_TZ = config('USE_TZ', default=True, cast=bool)
 
-STATIC_ROOT = config('STATIC_ROOT', default=os.path.join(BASE_DIR, 'static'))
-STATIC_URL = config('STATIC_URL', '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_ROOT = config('MEDIA_ROOT', default=os.path.join(BASE_DIR, 'media'))
