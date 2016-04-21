4adf82ff17d3d0383d6c7f1c23b32ea4fd932078
Anthony Zhang <azhang9@gmail.com>
2016-04-21 14:31:40 -0700

    Add missing files

---
+++ analysis_service/base/static/js/dashboard.js
@@ -0,0 +1,21 @@
+$(function() {
+  // set up form element popovers
+  $('[data-toggle="popover"]').popover();
+
+  // apply validation for form controls
+  $('input, select, textarea').not('[type=submit]').jqBootstrapValidation();
+
+  // apply datetimepicker initialization
+  $('.datetimepicker').datetimepicker({
+    sideBySide: true, // show the time picker and date picker at the same time
+    useCurrent: false, // don't automatically set the date when opening the dialog
+    widgetPositioning: {vertical: 'bottom'}, // make sure the picker shows up below the control
+    format: 'YYYY-MM-DD h:mm',
+  });
+
+  $(".editable-table").click(function() {
+    // allow selecting individual rows
+    $(this).parents("table").first().find("tr").removeClass("selected");
+    $(this).addClass("selected");
+  });
+});
