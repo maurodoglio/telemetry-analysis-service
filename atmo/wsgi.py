bbd0cb7fa74bbf41560875fcb04bf7a2fbf9e71d
Jannis Leidel <jannis@leidel.info>
2016-10-07 11:06:44 +0200

    Remove duplicate newrelic config

--- atmo/wsgi.py
+++ atmo/wsgi.py
@@ -11,16 +11,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atmo.settings')  # NOQA
 
 from django.core.wsgi import get_wsgi_application
 
-import newrelic.agent
-from decouple import config
-
-
 application = get_wsgi_application()
-
-# Add NewRelic
-default_ini = os.path.join(os.path.dirname(__file__), '..', 'newrelic.ini')
-newrelic_ini = config('NEW_RELIC_CONFIG_FILE', default=default_ini)
-newrelic_license_key = config('NEW_RELIC_LICENSE_KEY', default=None)
-if newrelic_ini and newrelic_license_key:
-    newrelic.agent.initialize(newrelic_ini)
-    application = newrelic.agent.wsgi_application()(application)
