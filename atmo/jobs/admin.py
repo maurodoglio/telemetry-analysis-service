dd303cc5350d15fb0cb3b948299cdd9c46a0731f
Mauro Doglio <mdoglio@mozilla.com>
2016-10-20 19:23:34 +0100

    Bug 1311760 - Add custom admin site for Cluster and SparkJob

--- atmo/jobs/admin.py
+++ atmo/jobs/admin.py
@@ -3,6 +3,27 @@ from django.contrib import admin
 from .models import SparkJob
 
 
+def run_now(modeladmin, request, queryset):
+    for job in queryset:
+        job.run()
+
+
 @admin.register(SparkJob)
 class SparkJobAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'identifier',
+        'size',
+        'created_by',
+        'start_date',
+        'end_date',
+        'last_run_date',
+        'is_enabled',
+        'most_recent_status',
+    ]
+    list_filter = [
+        'most_recent_status',
+        'size',
+        'is_enabled',
+    ]
+    search_fields = ['identifier', 'is_enabled', 'create_by__email', 'most_recent_status']
+    actions = [run_now, ]
