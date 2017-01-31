7ac1a05eb48e9979168d912bf8fd58eafd8c5087
Jannis Leidel <jannis@leidel.info>
2017-01-31 15:14:37 +0100

    Bug 1321358 - Minor rename of tooltip JS.

--- atmo/static/js/base.js
+++ atmo/static/js/base.js
@@ -1,12 +1,18 @@
 $(function() {
   AtmoCallbacks = $.Callbacks();
-  var tooltip = function() {
+
+  // load Bootstrap tooltips bubbles
+  var atmoTooltips = function() {
     $('[data-toggle="tooltip"]').tooltip();
+  };
+
+  // load Bootstrap confirmation
+  var atmoConfirmations = function() {
     $('[data-toggle="confirmation"]').confirmation({
       rootSelector: '[data-toggle="confirmation"]',
     });
   };
-  AtmoCallbacks.add(tooltip);
+
   // jump to tab if tab name is found in document location
   var atmoTabs = function() {
     var url = document.location.toString();
@@ -16,7 +22,7 @@ $(function() {
     }
 
     // prevent scrolling
-    $('.nav-tabs a').on('shown.bs.tab', function(event) {
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function(event) {
       if (history.pushState) {
           history.pushState(null, null, event.target.hash);
       } else {
@@ -25,6 +31,8 @@ $(function() {
     });
   };
 
+  AtmoCallbacks.add(atmoTooltips);
+  AtmoCallbacks.add(atmoConfirmations);
   AtmoCallbacks.add(atmoTabs);
   $(document).ready(function() {
     AtmoCallbacks.fire();
