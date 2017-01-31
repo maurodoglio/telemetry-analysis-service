baddbd457d251fe1441d432c4566570da7823c40
Jannis Leidel <jannis@leidel.info>
2017-01-31 15:14:18 +0100

    Bug 1321358 - Load tabs on reload via location hash.

--- atmo/static/js/base.js
+++ atmo/static/js/base.js
@@ -7,6 +7,25 @@ $(function() {
     });
   };
   AtmoCallbacks.add(tooltip);
+  // jump to tab if tab name is found in document location
+  var atmoTabs = function() {
+    var url = document.location.toString();
+    // show tab matching the document hash
+    if (url.match('#')) {
+      $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+    }
+
+    // prevent scrolling
+    $('.nav-tabs a').on('shown.bs.tab', function(event) {
+      if (history.pushState) {
+          history.pushState(null, null, event.target.hash);
+      } else {
+          window.location.hash = event.target.hash; //Polyfill for old browsers
+      }
+    });
+  };
+
+  AtmoCallbacks.add(atmoTabs);
   $(document).ready(function() {
     AtmoCallbacks.fire();
   });
